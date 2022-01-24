d9a698e5cdc2f279d8e588e2b85f1f2cfe4ce404
Pawel Furman <pawel.fur@yggdrasilgaming.com>
2022-01-24 01:58:49 +0100

    Fix typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: "https://pokeapi.co/api/v2/",
+  api: "https://pokeapi.co/api/v2",
   storage: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
 }
